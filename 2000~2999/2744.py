85a851156169c55e00eb0049a4f2d84b5acdc66d
merryn <h1335@naver.com>
2020-01-25 23:49:58 +0900

    new file

---
+++ 2000~2999/2744.py
@@ -0,0 +1,12 @@
+# s = input()
+
+# for i in s:
+#     if i.isupper():
+#         s = i.lower()
+#         print(s, end='')
+    
+#     else:
+#         s = i.upper()
+#         print(s, end='')
+
+print(input().swapcase())
