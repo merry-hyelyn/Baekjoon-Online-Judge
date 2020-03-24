766aadabcab8b6a89c145ce79b2ed78a1f28d815
merryn <h1335@naver.com>
2020-03-24 23:52:18 +0900

    1978.py

---
+++ 1000~1999/1978.py
@@ -0,0 +1,22 @@
+# 문제
+# 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다.
+# 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+
+# 출력
+# 주어진 수들 중 소수의 개수를 출력한다.
+
+num = [int(x) for x in input().split()]
+s = []
+print(num)
+for k in num:
+    count = 0
+    for j in range(1, k + 1):
+        if k % j == 0:
+            count += 1
+    if count == 2:
+        s.append(k)
+
+print(len(s))
