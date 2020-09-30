16dd92c4e75968aad8cb5015be9aa6a50f62365d
merry-hyelyn <h1335@naver.com>
2020-09-30 22:18:39 +0900

    2609.py

--- 2000~2999/2609.py
+++ 2000~2999/2609.py
@@ -7,28 +7,13 @@
 # 출력
 # 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
 
-def make_factor(num):
-    n = int(num**0.5)
-    result = []
-
-    for i in range(2, n+1):
-        if num % i == 0:
-            result.extend((i, int(num/i)))
-
-    return result
-
-
 a, b = map(int, input().split())
 
-factor_a = set(make_factor(a))
-factor_b = set(make_factor(b))
+if (b > a):
+    a, b = b, a
 
-factor = factor_a.intersection(factor_b)
+while(b != 0):
+    a = a % b
+    a, b = b, a
 
-multiple_a = set([a*i for i in range(2, max(factor))])
-multiple_b = set([b*i for i in range(2, max(factor))])
-
-multiple = multiple_a.intersection(multiple_b)
-
-print(max(factor))
-print(min(multiple))
+print(a)
