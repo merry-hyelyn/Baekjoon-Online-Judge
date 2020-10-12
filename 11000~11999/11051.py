f4ad3511eea70f9aca8ff4d0ffb849fc89e6d763
merry-hyelyn <h1335@naver.com>
2020-10-12 20:20:44 +0900

    1105.py

--- 11000~11999/11051.py
+++ 11000~11999/11051.py
@@ -11,6 +11,6 @@ from math import factorial
 
 n, k = map(int, input().split())
 
-x = factorial(n) / (factorial(n-k) * factorial(k))
+x = factorial(n) // (factorial(n-k) * factorial(k))
 
 print(int(x % 10007))
