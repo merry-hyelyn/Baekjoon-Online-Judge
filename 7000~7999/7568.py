192b63a96232f9e2680e2a558312191151b89110
merry-hyelyn <h1335@naver.com>
2020-12-14 21:06:51 +0900

    7568 -ing

--- 7000~7999/7568.py
+++ 7000~7999/7568.py
@@ -1,5 +1,15 @@
-n, m = map(int, input().split())
-weight_height = list(map(int, input().split()))
+from operator import itemgetter
+
+n = int(input())
+weight_height = [list(map(int, input().split())) for _ in range(n)]
+
+weight_height.sort(key=itemgetter(0, 1), reverse=True)
 grade = 1
+result = {}
+
 for i in range(n):
-    pass
+    if i == 0:
+        result[weight_height[i]] = 1
+    else:
+        prev_w, prev_h = weight_height[i-1]
+        w, h = weight_height[i]
