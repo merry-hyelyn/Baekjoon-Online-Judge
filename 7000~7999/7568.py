e03a120f9851990de0d5007236db7709c35e7f66
merry-hyelyn <h1335@naver.com>
2020-12-15 22:54:49 +0900

    7568 -ing

--- 7000~7999/7568.py
+++ 7000~7999/7568.py
@@ -1,15 +1,22 @@
 from operator import itemgetter
 
 n = int(input())
-weight_height = [list(map(int, input().split())) for _ in range(n)]
-
-weight_height.sort(key=itemgetter(0, 1), reverse=True)
 grade = 1
 result = {}
-
+weight_height = [list(map(int, input().split())) for _ in range(n)]
+sort_wh_list = sorted(weight_height, key=itemgetter(0, 1), reverse=True)
 for i in range(n):
+    index = weight_height.index(sort_wh_list[i])
     if i == 0:
-        result[weight_height[i]] = 1
+        result[index] = grade
     else:
-        prev_w, prev_h = weight_height[i-1]
-        w, h = weight_height[i]
+        prev_w, prev_h = sort_wh_list[i-1]
+        w, h = sort_wh_list[i]
+        if prev_h > h:
+            grade = i + 1
+        result[index] = grade
+
+for i, wh in enumerate(weight_height):
+    print(result[i], end='')
+    if i != n-1:
+        print(' ', end='')
